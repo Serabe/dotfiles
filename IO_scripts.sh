1ffd77965c0e0d3656dbeb1dc56e4bec0ed0f616
Sergio Arbeo <serabe@gmail.com>
2015-03-08 23:08:18 +0100

    Remove apsd

--- IO_scripts.sh
+++ IO_scripts.sh
@@ -1,2 +1,6 @@
 # 0 is fastest and 3 is slowest
 defaults write NSGlobalDomain KeyRepeat -int 1
+# disable apsd (Apple Push Notification Servide)
+sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.apsd.plist
+# reenable it with:
+# sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.apsd.plist w
