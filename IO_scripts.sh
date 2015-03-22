21b103bd41a9d0c1838faf7f47936674d3969309
Sergio Arbeo <serabe@gmail.com>
2015-03-22 15:59:22 +0100

    Improve IO_script.

--- IO_scripts.sh
+++ IO_scripts.sh
@@ -1,6 +1,12 @@
+# Make sure zsh is in /etc/shells
+echo $(which zsh) >> /etc/shells
+# Change shell to chsh
+chsh -s $(which zsh)
 # 0 is fastest and 3 is slowest
 defaults write NSGlobalDomain KeyRepeat -int 1
 # disable apsd (Apple Push Notification Servide)
 sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.apsd.plist
 # reenable it with:
 # sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.apsd.plist w
+
+# Set Inconsolata-dz for Powerline in iTerm for bot ASCII and non-ASCII chars
